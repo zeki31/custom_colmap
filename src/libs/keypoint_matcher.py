0a2d9f0de2d0401db57b749da7859b20013c9b29
zeki31 <ozeki.f.h.13@gmail.com>
2025-06-24 08:26:37 +0000

    feat: read mask images

--- src/libs/keypoint_matcher.py
+++ src/libs/keypoint_matcher.py
@@ -61,7 +61,7 @@ def keypoint_distances(
 
             # If mask is enabled, remove the matches that are in the mask
             if cfg.mask:
-                mask_img1 = cv2.imread(mask_dir / key1, cv2.IMREAD_GRAYSCALE)
+                mask_img1 = cv2.imread(mask_dir / paths[idx1].name, cv2.IMREAD_GRAYSCALE)
                 mask_img1 = torch.from_numpy(mask_img1).to(device)
 
                 # Get the pixel positions of the matches
