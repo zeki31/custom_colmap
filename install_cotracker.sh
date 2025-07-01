db2511d1bf17988cc4f77ecfe01e2dbc1d4be53d
zeki31 <ozeki.f.h.13@gmail.com>
2025-07-01 18:41:26 +0900

    feat: add co-tracker submodule and installation script; update dependencies

---
+++ install_cotracker.sh
@@ -0,0 +1,8 @@
+cd src/libs/submodules/co-tracker
+mkdir -p checkpoints
+cd checkpoints
+# download the online (multi window) model
+wget https://huggingface.co/facebook/cotracker3/resolve/main/scaled_online.pth
+# download the offline (single window) model
+wget https://huggingface.co/facebook/cotracker3/resolve/main/scaled_offline.pth
+cd ../../../..
